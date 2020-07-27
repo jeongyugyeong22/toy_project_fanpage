e7f0a67d4cc770b9721020691ac931f58e5c944c
jeongyugyeong22 <jyg1022@naver.com>
2020-07-27 17:00:39 +0900

    add javascript

---
+++ script/listScript.js
@@ -0,0 +1,12 @@
+var list = document.querySelector(".movieList");
+
+for (var i = 23; i > 0; i--) {
+    var element = document.createElement("li");
+    element.className = "list";
+    var img = document.createElement("img");
+    img.className = "movieImg";
+    img.src = '../image/' + i + '.jpg';
+    img.onclick = function () {popUp()};
+    list.appendChild(element);
+    element.appendChild(img);
+}
